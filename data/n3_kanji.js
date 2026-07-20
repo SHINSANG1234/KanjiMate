224c5e5f43d75e1e9a8cabb2ec4115d39f8c9a52
SHINSANG1234 <gs01045420470@gmail.com>
2026-07-20 15:02:20 +0900

    Add n3 kanji data

---
+++ data/n3_kanji.js
@@ -0,0 +1,25 @@
+const n3Kanji = [
+
+{
+kanji:"学",
+meaning:"배우다",
+onyomi:"がく",
+kunyomi:"まなぶ",
+words:[
+"学校（がっこう） 학교",
+"学生（がくせい） 학생"
+]
+},
+
+{
+kanji:"生",
+meaning:"나다, 살다",
+onyomi:"せい",
+kunyomi:"いきる",
+words:[
+"先生（せんせい） 선생님",
+"生活（せいかつ） 생활"
+]
+}
+
+];
